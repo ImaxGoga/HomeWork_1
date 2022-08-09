5e86827fe3564c443b199f9c52a9d82f6a3cab24
ImaxGoga <45959194+ImaxGoga@users.noreply.github.com>
2022-08-09 22:49:26 +0300

    1_1_ver3.0

--- 1_1/Program.cs
+++ 1_1/Program.cs
@@ -2,11 +2,28 @@
 int a = int.Parse(Console.ReadLine());
 Console.Write("Введите число b: ");
 int b = int.Parse(Console.ReadLine());
-if ( a > b)
+if ( a == b)
 {
-    Console.WriteLine(a);
+    Console.Write("Число ");
+    Console.Write(a);
+    Console.Write(" равно числу ");
+    Console.Write(b);
+}
+else {
+    if ( a > b)
+{
+    Console.Write("Число ");
+    Console.Write(a);
+    Console.Write(" больше чем число ");
+    Console.Write(b);
 }
 else 
 {
-    Console.WriteLine(b);
+    Console.Write("Число ");
+    Console.Write(b);
+    Console.Write(" больше чем число ");
+    Console.Write(a);
+    
 }
+}
+
